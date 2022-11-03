752277af839a88ea6df734fbd594b8a975575d3f
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2022-11-03 18:32:19 +0100

    bundle type declarations with `vite-plugin-dts`

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import typescript from "@rollup/plugin-typescript"
 import vue from "@vitejs/plugin-vue"
 import { resolve } from "path"
 import { defineConfig } from "vite"
+import dts from "vite-plugin-dts"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), typescript({ tsconfig: "./tsconfig.json" })],
+  plugins: [vue(), dts({ rollupTypes: true })],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
