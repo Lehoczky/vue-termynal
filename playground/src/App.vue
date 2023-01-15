2fce65447e006286be8c754f88ec805d3e3920b8
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2023-01-15 15:27:45 +0100

    add playground example for unmounting the termynal

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="max-w-5xl mx-auto px-10">
     <header class="flex gap-3 sticky bg-white top-0 py-2 mb-1">
-      <router-link to="/">Basic</router-link>
-      <router-link to="/scrolling-terminal">Autoscroll</router-link>
-      <router-link class="" to="/intersection-observer"
+      <router-link class="button" to="/">Basic</router-link>
+      <router-link class="button" to="/scrolling-terminal"
+        >Autoscroll</router-link
+      >
+      <router-link class="button" to="/intersection-observer"
         >IntersectionObserver</router-link
+      >
+      <router-link class="button" to="/unmount-during-animation"
+        >Unmount during animation</router-link
       >
     </header>
 
---
+++ playground/src/pages/unmount-during-animation.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <button class="button my-5" @click="toggleTermynal">
+      {{ buttonText }}
+    </button>
+
+    <v-termynal v-if="showTermynal" forward-button restart-button>
+      <vt-input prompt="▲">npm uninstall react</vt-input>
+      <vt-text>Are you sure you want to uninstall 'react'?</vt-text>
+      <vt-input prompt="(y/n)" :type-delay="1000">y</vt-input>
+      <vt-progress progress-char="·" />
+      <vt-text>Uninstalled 'react'</vt-text>
+      <vt-input prompt="▲">node</vt-input>
+      <vt-input prompt=">">Array(5).fill('🦄 ')</vt-input>
+      <vt-text>['🦄', '🦄', '🦄', '🦄', '🦄']</vt-text>
+      <vt-input prompt="▲">cd ~/repos</vt-input>
+      <vt-input prompt="▲  ~/repos">git checkout branch master</vt-input>
+      <vt-input prompt="▲  ~/repos (master)"
+        >git commit -m "Fix things"</vt-input
+      >
+    </v-termynal>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from "vue"
+
+const showTermynal = ref(true)
+
+function toggleTermynal() {
+  showTermynal.value = !showTermynal.value
+}
+const buttonText = computed(() =>
+  showTermynal.value ? "Unmount Termynal" : "Mount Termynal"
+)
+</script>
