3abe5803948194789f1a19ff7c99b22eb0e7ae32
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2025-04-08 18:32:32 +0200

    migrate the playground to tailwind v4

--- packages/playground/tailwind.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "tailwindcss"
-
-export default {
-  content: ["./src/**/*.vue"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-} satisfies Config
--- packages/playground/vite.config.ts
+++ packages/playground/vite.config.ts
@@ -1,8 +1,9 @@
+import tailwindcss from "@tailwindcss/vite"
 import vue from "@vitejs/plugin-vue"
 import { defineConfig } from "vite"
 import Pages from "vite-plugin-pages"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Pages()],
+  plugins: [vue(), tailwindcss(), Pages()],
 })
